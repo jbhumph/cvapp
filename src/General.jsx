1d45ea5416bf997e7a9da9dd81b35cd75e389420
John <jbhumph@gmail.com>
2025-04-09 15:33:36 -0700

    Added initial state functionality to general

--- src/General.jsx
+++ src/General.jsx
@@ -1,19 +1,54 @@
+import React, { useState } from 'react';
+
 function General() {
+  const [isEditing, setIsEditing] = useState(true);
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone: ''
+  });
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormData({
+      ...formData,
+      [id]: value
+    });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsEditing(false);
+  };
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  }
+  
   return (
     <div className="general">
       <h1>General</h1>
-      <form>
-        <label for="name">Name:</label><br />
-        <input type="text" id="name"></input><br />
-        <label for="email">Email:</label><br />
-        <input type="email" id="email"></input><br />
-        <label for="phone">Phone Number:</label><br />
-        <input type="tel" id="phone"></input><br />
+      {isEditing ? (
+        <form onSubmit={handleSubmit}>
+          <label for="name">Name:</label><br />
+          <input type="text" id="name" value={formData.name} onChange={handleChange}></input><br />
+          <label for="email">Email:</label><br />
+          <input type="email" id="email" value={formData.email} onChange={handleChange}></input><br />
+          <label for="phone">Phone Number:</label><br />
+          <input type="tel" id="phone" value={formData.phone} onChange={handleChange}></input><br />
+          <div>
+            <button type="submit">Submit</button>
+            <button type="button">Edit</button>
+          </div>
+        </form>
+      ) : (
         <div>
-          <button type="submit">Submit</button>
-          <button type="button">Edit</button>
+          <p>Name: {formData.name}</p>
+          <p>Email: {formData.email}</p>
+          <p>Phone Number: {formData.phone}</p>
+          <button onClick={handleEdit}>Edit</button>
         </div>
-      </form>
+      )}
     </div>
   );
 }
