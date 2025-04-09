595fec18142743302ce5513491cca8b37f5aac5a
John <jbhumph@gmail.com>
2025-04-09 15:58:57 -0700

    Added initial state logic to Educational and Practical

--- src/Educational.jsx
+++ src/Educational.jsx
@@ -1,19 +1,54 @@
+import React, { useState } from 'react';
+
 function Educational() {
+  const [isEditing, setIsEditing] = useState(true);
+  const [formData, setFormData] = useState({
+    schoolname: '',
+    degree: '',
+    gpa: ''
+  });
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormData({
+      ...formData,
+      [id]: value
+    });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsEditing(false);
+  };
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  }
+  
   return (
     <div className="educational">
       <h1>Educational</h1>
-      <form>
-        <label for="schoolname">School Name:</label><br />
-        <input type="text" id="schoolname"></input><br />
-        <label for="degree">Area of Study:</label><br />
-        <input type="text" id="degree"></input><br />
-        <label for="gpa">GPA:</label><br />
-        <input type="number" id="gpa"></input><br />
+      {isEditing ? (
+        <form onSubmit={handleSubmit}>
+          <label for="schoolname">School Name:</label><br />
+          <input type="text" id="schoolname" value={formData.schoolname} onChange={handleChange}></input><br />
+          <label for="degree">Area of Study:</label><br />
+          <input type="text" id="degree" value={formData.degree} onChange={handleChange}></input><br />
+          <label for="gpa">GPA:</label><br />
+          <input type="number" id="gpa" value={formData.gpa} onChange={handleChange}></input><br />
+          <div>
+            <button type="submit">Submit</button>
+            <button type="button">Edit</button>
+          </div>
+        </form>
+      ) : (
         <div>
-          <button type="submit">Submit</button>
-          <button type="button">Edit</button>
+          <p>School Name: {formData.schoolname}</p>
+          <p>Area of Study: {formData.degree}</p>
+          <p>GPA: {formData.gpa}</p>
+          <button onClick={handleEdit}>Edit</button>
         </div>
-      </form>
+      )}
     </div>
   );
 }
--- src/Practical.jsx
+++ src/Practical.jsx
@@ -1,23 +1,62 @@
+import React, { useState } from 'react';
+
 function Practical() {
+  const [isEditing, setIsEditing] = useState(true);
+    const [formData, setFormData] = useState({
+      companyname: '',
+      position: '',
+      tasks: '',
+      startdate: '',
+      enddate: ''
+    });
+  
+    const handleChange = (e) => {
+      const { id, value } = e.target;
+      setFormData({
+        ...formData,
+        [id]: value
+      });
+    }
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      setIsEditing(false);
+    };
+  
+    const handleEdit = () => {
+      setIsEditing(true);
+    }
+  
   return (
     <div className="practical">
       <h1>Practical</h1>
-      <form>
-        <label for="companyname">Company Name:</label><br />
-        <input type="text" id="companyname"></input><br />
-        <label for="position">Position Held:</label><br />
-        <input type="text" id="position"></input><br />
-        <label for="tasks">Responsibilities:</label><br />
-        <textarea id="tasks"></textarea><br />
-        <label for="startdate">Start Date:</label><br />
-        <input type="date" id="startdate"></input><br />
-        <label for="enddate">End Date:</label><br />
-        <input type="date" id="enddate"></input><br />
+      {isEditing ? (
+        <form onSubmit={handleSubmit}>
+          <label for="companyname">Company Name:</label><br />
+          <input type="text" id="companyname" value={formData.companyname} onChange={handleChange}></input><br />
+          <label for="position">Position Held:</label><br />
+          <input type="text" id="position" value={formData.position} onChange={handleChange}></input><br />
+          <label for="tasks">Responsibilities:</label><br />
+          <textarea id="tasks" value={formData.tasks} onChange={handleChange}></textarea><br />
+          <label for="startdate">Start Date:</label><br />
+          <input type="date" id="startdate" value={formData.startdate} onChange={handleChange}></input><br />
+          <label for="enddate">End Date:</label><br />
+          <input type="date" id="enddate" value={formData.enddate} onChange={handleChange}></input><br />
+          <div>
+            <button type="submit">Submit</button>
+            <button type="button">Edit</button>
+          </div>
+        </form>
+      ) : (
         <div>
-          <button type="submit">Submit</button>
-          <button type="button">Edit</button>
+          <p>Company Name: {formData.companyname}</p>
+          <p>Position Held: {formData.position}</p>
+          <p>Responsibilities: {formData.tasks}</p>
+          <p>Start Date: {formData.startdate}</p>
+          <p>End Date: {formData.enddate}</p>
+          <button onClick={handleEdit}>Edit</button>
         </div>
-      </form>
+      )}
     </div>
   );
 }
